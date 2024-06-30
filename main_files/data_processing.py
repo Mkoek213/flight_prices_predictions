b41759070f15a32b075dc2499caa35289b4895d9
Mkoek213 <mikolaj.kolek2@gmail.com>
2024-06-30 22:53:45 +0200

    added process_time_data func

--- main_files/data_processing.py
+++ main_files/data_processing.py
@@ -5,7 +5,7 @@ from main_files import np
 from main_files import pd
 import io
 import sys
-
+from datetime import datetime
 
 def print_data_info(database):
     buffer = io.StringIO()
@@ -114,19 +114,100 @@ def one_hot_encoding_category_data(database):
     flight_data_processed.drop('Airline', axis=1, inplace=True)
 
     # One hot encoding 'Source' parameter
-    value_counts_source = flight_data_processed['Source'].value_counts()
     df_encoded_source = pd.get_dummies(flight_data_processed['Source'], prefix='Source').astype(int)
     flight_data_processed = pd.concat([flight_data_processed, df_encoded_source], axis=1)
     flight_data_processed.drop('Source', axis=1, inplace=True)
 
     # One hot encoding 'Destination' parameter
-    value_counts_destination = flight_data_processed['Destination'].value_counts()
     df_encoded_destination = pd.get_dummies(flight_data_processed['Destination'], prefix='Destination').astype(int)
     flight_data_processed = pd.concat([flight_data_processed, df_encoded_destination], axis=1)
     flight_data_processed.drop('Destination', axis=1, inplace=True)
 
     return flight_data_processed
 
+def categorize_hour(hour):
+    if 5 <= hour < 12:
+        return 'Morning'
+    elif 12 <= hour < 17:
+        return 'Afternoon'
+    elif 17 <= hour < 21:
+        return 'Evening'
+    elif 21 <= hour <= 23 or 0 <= hour < 5:
+        return 'Night'
+    else:
+        return 'Invalid Hour'
+
+def calculate_day_of_week(row):
+    date = datetime(year=2019, month=row['Month'], day=row['Date'])
+    return date.strftime('%A')
+
+def process_time_data(database):
+    flight_data_processed = database.copy()
+    # Counting total duration time
+    flight_data_processed['Total_Duration_min'] = flight_data_processed['Duration_hours'] * 24 + flight_data_processed['Duration_min']
+    flight_data_processed.drop('Duration_hours', axis=1, inplace=True)
+    flight_data_processed.drop('Duration_min', axis=1, inplace=True)
+
+    # Categorizing 'Dep_hours' and 'Arrival_hours'
+    flight_data_processed['Departure_Time_categorized'] = flight_data_processed['Dep_hours'].apply(categorize_hour)
+    flight_data_processed['Arrival_Time_categorized'] = flight_data_processed['Arrival_hours'].apply(categorize_hour)
+    flight_data_processed.drop(columns=['Dep_hours', 'Arrival_hours', 'Dep_min', 'Arrival_min'], inplace=True, axis=1)
+
+    # One hot encoding 'Departure_Time_categorized'
+    df_encoded_dep = pd.get_dummies(flight_data_processed['Departure_Time_categorized'], prefix='Departure').astype(int)
+    flight_data_processed = pd.concat([flight_data_processed, df_encoded_dep], axis=1)
+    flight_data_processed.drop('Departure_Time_categorized', axis=1, inplace=True)
+
+    # One hot encoding 'Arrival_Time_categorized'
+    df_encoded_arrival = pd.get_dummies(flight_data_processed['Arrival_Time_categorized'], prefix='Arrival').astype(int)
+    flight_data_processed = pd.concat([flight_data_processed, df_encoded_arrival], axis=1)
+    flight_data_processed.drop('Arrival_Time_categorized', axis=1, inplace=True)
+
+    # Mapping month
+    month_mapping = {
+        1: 'January', 2: 'February', 3: 'March', 4: 'April',
+        5: 'May', 6: 'June', 7: 'July', 8: 'August',
+        9: 'September', 10: 'October', 11: 'November', 12: 'December'
+    }
+
+    flight_data_processed['Month_Name'] = flight_data_processed['Month'].map(month_mapping)
+
+    # One hot encoding 'Month_Name'
+    df_encoded_month = pd.get_dummies(flight_data_processed['Month_Name'], prefix='Month').astype(int)
+    flight_data_processed = pd.concat([flight_data_processed, df_encoded_month], axis=1)
+    flight_data_processed.drop('Month_Name', axis=1, inplace=True)
+
+    # Calculate day of week
+    flight_data_processed['Day_of_Week'] = flight_data_processed.apply(calculate_day_of_week, axis=1)
+
+    # One hot encoding 'Day_of_Week'
+    df_encoded_day = pd.get_dummies(flight_data_processed['Day_of_Week'], prefix='Day').astype(int)
+    flight_data_processed = pd.concat([flight_data_processed, df_encoded_day], axis=1)
+    flight_data_processed.drop(columns=['Day_of_Week', 'Date', 'Month'], axis=1, inplace=True)
+    return flight_data_processed
+
+def categorize_day(day):
+    if 5 <= day < 12:
+        return 'first_week'
+    elif 12 <= day < 17:
+        return 'second_week'
+    elif 17 <= day < 21:
+        return 'third_week'
+    elif 21 <= day <= 23:
+        return 'fourth_week'
+    else:
+        return 'Invalid Hour'
+
+
+
 
 processed_data = one_hot_encoding_category_data(flight_data)
-print_data_info(processed_data)
+# print_data_info(processed_data)
+# plot_for_outliers(flight_data)
+# plot_correlation_heatmap(flight_data)
+# plot_distribution_info(flight_data)
+# plot_category_data_distribution(flight_data)
+processed_data2 = process_time_data(processed_data)
+print_data_info(processed_data2)
+
+print(len(processed_data2.columns))
