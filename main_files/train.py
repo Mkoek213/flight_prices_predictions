dbeadc85d6a10d42f3713e1879c09d4bbab483ef
Mkoek213 <mikolaj.kolek2@gmail.com>
2024-07-23 08:40:22 +0200

    added train_tree_regressor function, train.py done

--- main_files/train.py
+++ main_files/train.py
@@ -1,3 +1,4 @@
+from main_files import DecisionTreeRegressor
 from main_files import XGBRegressor
 from main_files import np
 from main_files import GridSearchCV
@@ -126,5 +127,73 @@ def train_xgbregressor(X_train, X_test, y_train, y_test):
 
     # Save the trained model to a file
     model_path = 'models/xgbregressor_model.joblib'
+    joblib.dump(model, model_path)
+    print(f"Model saved to {model_path}")
+
+def train_tree_regressor(X_train, X_test, y_train, y_test):
+    # Define parameter distributions for RandomizedSearchCV
+    parameters = {
+        'splitter': ['random'],
+        'max_depth': sp_randInt(10, 25),
+        'min_samples_split': sp_randInt(15, 50),
+        'max_features': np.linspace(800, 1500, 25).astype(int),
+        'criterion': ['friedman_mse'],
+        'min_samples_leaf': sp_randInt(1, 5),
+        'min_weight_fraction_leaf': sp_randFloat(0, 0.001),
+        'max_leaf_nodes': sp_randInt(100, 250),
+        'min_impurity_decrease': sp_randFloat(0.1, 0.6)
+    }
+
+    # Initialize RandomizedSearchCV with DecisionTreeRegressor and the parameter distributions
+    random_search = RandomizedSearchCV(
+        estimator=DecisionTreeRegressor(),
+        n_iter = 100,
+        param_distributions = parameters,
+        scoring='r2',
+        cv=5,
+        n_jobs=-1,
+        verbose=4,
+        random_state = 1
+    )
+
+    # Start timing for model training
+    start_time = time.time()
+
+    # Perform Randomized Search to find the best parameters
+    random_search.fit(X_train, y_train)
+
+    # End timing
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    # Retrieve the best parameters and score from Randomized Search
+    best_params = random_search.best_params_
+    best_score = random_search.best_score_
+
+    # Train the DecisionTreeRegressor model with the best parameters
+    model = DecisionTreeRegressor(**best_params)
+    model.fit(X_train, y_train)
+
+    # Predict on training and testing data
+    y_train_pred = model.predict(X_train)
+    y_test_pred = model.predict(X_test)
+
+    # Calculate performance metrics
+
+    train_mse = mean_squared_error(y_train, y_train_pred)
+    test_mse = mean_squared_error(y_test, y_test_pred)
+    train_r2 = r2_score(y_train, y_train_pred)
+    test_r2 = r2_score(y_test, y_test_pred)
+
+    # Print model performance and training time
+    print(f"Best score: {best_score}")
+    print(f"Training time: {elapsed_time:.2f} seconds")
+    print(f"Train MSE: {train_mse}")
+    print(f"Test MSE: {test_mse}")
+    print(f"Train R^2: {train_r2}")
+    print(f"Test R^2: {test_r2}")
+
+    # Save the trained model to a file
+    model_path = 'models/tree_regressor_model.joblib'
     joblib.dump(model, model_path)
     print(f"Model saved to {model_path}")
